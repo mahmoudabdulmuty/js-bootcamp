9d8081dac0978b17884a737070920777570c48a5
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-18 15:19:04 +0200

    adding temp-conversion.js

---
+++ 00.basics/temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheit = 32; // 0c - 273.15k
+
+// calcualte celsius and store in celsius variable
+let celsius = (fahrenheit - 32) * (5 / 9);
+// print that value
+console.log('Celsius =', celsius + 'c');
+
+// calcualte kelvin and store in kelvin variable
+let kelvin = (fahrenheit + 459.67) * (5 / 9);
+// print that value
+console.log('Kelvin =', kelvin + 'k');
