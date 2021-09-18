57908ddf6023777f222e0c6ef47cd6c62b7f7f20
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-18 10:46:08 +0200

    adding numbers.js

---
+++ 00. basics/numbers.js
@@ -0,0 +1,16 @@
+let age = 28;
+
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+// Challenge area
+
+// declaring studentScore
+let studentScore = 18;
+// declaring maxScore
+let maxScore = 20;
+// declaring percent
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent);
