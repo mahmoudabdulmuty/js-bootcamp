9a5b11e8bd5ff49cecb52ee0cb0e7085f944f9c0
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-20 11:45:50 +0200

    adding undefined-null.js

---
+++ 01-functions/undefined-null.js
@@ -0,0 +1,23 @@
+// undefined for vaiables
+let name;
+name = 'Jen';
+
+if (name === undefined) {
+	console.log(name);
+} else {
+	console.log(name);
+}
+
+// undefined for function arguments
+// undefined as function return default value
+let value = function (num) {
+	return num;
+};
+let result = value();
+console.log(result);
+
+// Null as assigned value
+
+let age = 28;
+age = null;
+console.log(age);
