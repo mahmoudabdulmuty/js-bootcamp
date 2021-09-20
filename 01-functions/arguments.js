a1e7f59db00b3e2aa5d6255f038eb897fe601cff
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-20 16:20:10 +0200

    updating arguments.js

--- 01-functions/arguments.js
+++ 01-functions/arguments.js
@@ -2,8 +2,9 @@
 
 let getTip = function (total, tipPercent = 0.2) {
 	let tipValue = tipPercent * total;
-	return `your tip is ${tipValue}`;
+	let percent = tipPercent * 100
+	return `A ${percent * 100}% tip on $${total} is $${tipValue}`;
 };
 
-let tip = getTip(100, 0.25);
+let tip = getTip(100);
 console.log(tip);
