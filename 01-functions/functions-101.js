6bdd9ae4b7b7a5228910d5ec4f98538138a96bc9
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-20 11:45:02 +0200

    adding functions-101.js

---
+++ 01-functions/functions-101.js
@@ -0,0 +1,16 @@
+// Functions input(argument) - code - output()
+
+let square = function (num) {
+	return num * 2;
+};
+// let value = square(2);
+// console.log(value);
+
+// Challenge Area
+
+let convertFahrenheitToCelsius = function (fahrenheit) {
+	return (fahrenheit - 32) * (5 / 9);
+};
+
+let value = convertFahrenheitToCelsius(68);
+console.log(value);
