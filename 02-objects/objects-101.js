946d4dac1ea85d7f3362c09cb0fb855c0eeee5e4
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-21 20:51:38 +0200

    adding objects-101.js

---
+++ 02-objects/objects-101.js
@@ -0,0 +1,19 @@
+let myBook = {
+	title: '1984',
+	author: 'George Orwell',
+	pageCount: 326
+};
+console.log(`${myBook.title} by ${myBook.author}`);
+
+// Cahllenge Area
+const person = {
+	name: 'Mahmoud',
+	age: 28,
+	location: 'Cairo, Egypt'
+};
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`);
+
+person.age++;
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`);
