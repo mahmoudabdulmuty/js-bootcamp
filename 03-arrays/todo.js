bfc46d5fa6c5ffae5a4eeebdbf5c83c2bbd6bdf5
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-24 15:44:30 +0200

    adding notes.js & todo.js

---
+++ 03-arrays/todo.js
@@ -0,0 +1,9 @@
+const todos = [
+	'Sleep at least 8 Hours',
+	'Eat Well',
+	'Finish JS-Bootcamp Section',
+	'Complete JS-hero Challenge',
+	'Read at least 1 Chapeter'
+];
+console.log(`You have ${todos.length} todos`);
+console.log(`Todo#1: ${todos[0]} , Todo#2ndToLast: ${todos[todos.length - 2]}`);
