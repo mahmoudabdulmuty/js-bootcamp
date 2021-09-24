97d0ebe0b783ee68fb292a3518bfbfa87025e023
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-24 19:29:09 +0200

    updating notes.js

--- 03-arrays/notes.js
+++ 03-arrays/notes.js
@@ -1,3 +1,21 @@
-const notes = ['Note#1', 'Note#2', 'Note#3'];
-console.log(notes[0]);
-console.log(notes[notes.length - 1]);
+const notes = [
+	{ title: 'Note#1', body: 'Haha' },
+	{ title: 'Note#2', body: 'Laha' },
+	{ title: 'Note#3', body: 'Zaha' }
+];
+
+// finding note
+const findNote = function (notes, noteTitle) {
+	return notes.find(note => {
+		return note.title.toLowerCase() === noteTitle.trim().toLowerCase();
+	});
+};
+console.log(findNote(notes, ' note#2'));
+// finding note index
+const note = function (notes, noteTitle) {
+	const noteIndex = notes.findIndex(note => {
+		return note.title.toLowerCase() === noteTitle.trim().toLowerCase();
+	});
+	return notes[noteIndex];
+};
+console.log(note(notes, ' note#2'));
